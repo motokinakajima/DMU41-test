5879efcd15b04670bfecb7e23b1c81b38b7d1e05
Motoki Nakajima <motokinakajima0912@gmail.com>
2026-06-29 18:33:13 +0900

    added  health check

--- health_check.py
+++ health_check.py
@@ -23,6 +23,15 @@ def main():
                 baudrate=cfg["baud"],
                 timeout=0  # Non-blocking mode
             )
+            
+            if name == "SPRE":
+                serials[name].dtr = False
+                serials[name].rts = False
+                time.sleep(0.1)
+                serials[name].dtr = True
+                serials[name].rts = True
+                time.sleep(0.1)
+                
             print(f"[ OK ] Opened {name} on {cfg['path']}")
         except Exception as e:
             print(f"[FAIL] Could not open {name}: {e}")
